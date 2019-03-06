85deca1f3b17a6ea1ebf097d6d0b508664efa11e
Neil <nmccarter00@gmail.com>
2019-03-06 02:11:13 -0500

    ino coming together

--- src/sonde_node/sonde_node.ino
+++ src/sonde_node/sonde_node.ino
@@ -19,9 +19,18 @@ std_msgs::Int32 sig;
 //aro_refactored:: data_msg; // Need to add custom messages here TODO 
 std_msgs::Empty data_msg;
 
-void teensyStateCb (const std_msgs::Int32& sig)
+
+int STATE;
+
+void teensyStateCb (const std_msgs::Int32& newState)
 {
-  
+
+  Serial.print("STATE was ");
+  Serial.print(STATE);
+  Serial.print("\t now is ");
+  Serial.println(newState.data);
+  STATE = newState.data;
+  // Is that it?
 }
 
 
@@ -35,6 +44,7 @@ ros::Subscriber<std_msgs::Int32> teensy_state_sub("teensy_state", &teensyStateCb
 
 
 
+//int STATE; // So that its nonvolatile? Are callbacks interrupts? - they arent, entered when spin is called
 int msg_count;
 
 MS5837 bar30;
@@ -51,7 +61,12 @@ void setup() {
   // Subscribers
   nh.subscribe(teensy_state_sub);
 
+  // Might want to make teensy signals more detailed
+  sig.data = 0;
+
+
   msg_count = 0;
+  STATE = 0;
 
   Wire2.begin(); // Depth sensor, I2C 2
   bar30.init();
@@ -68,6 +83,40 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  nh.spinOnce(); // This is where STATE will be updated
+
+  switch (STATE)
+  {
+    case 0 : 
+      // Idle state, put a non-busy wait here
+      Serial.println("Chilling (state 0)");
+      delay(1000);
+      
+      break;
+    case 1 : 
+      // Sending heartbeats for testing comms
+      Serial.println("Sending hb");
+      teensy_signal_pub.publish(&sig); // Change sig to something first? Value to indicate heartbeat? May help with debugging
+      delay(500);
+
+      break;
+    case 2 : 
+      // Start collecting data! Repeat until another signal arrives 
+      Serial.println("Pretending to collect data brb");
+      delay(250);
+      
+      break;
+    case 3 : 
+
+      break;
+    case 4 : 
+
+      break;
+      
+  }
+
+
 
 }
 
