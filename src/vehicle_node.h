20e3ea15bcf649efb482d36129664db46d7c86c6
Neil <nmccarter00@gmail.com>
2019-03-12 18:12:55 -0400

    Added bool for torque sensing but decided to switch back

--- src/vehicle_node.h
+++ src/vehicle_node.h
@@ -12,89 +12,99 @@
 
 class DynamixelClient {
 
-    //All speeds are in rad/s
-    //Spool radius is ~1.5cm ~0.6in ~0.5ft
-    private:
-        ros::NodeHandle &nh_;
+//All speeds are in rad/s
+//Spool radius is ~1.5cm ~0.6in ~0.5ft
+private:
+	ros::NodeHandle &nh_;
 
-        float reel_speed;       // rad/s
-        float desired_depth;    // ft
-        float sample_time;      // seconds
-        float current_torque;   // motor torque
+	float reel_speed;       // rad/s
+	float desired_depth;    // ft
+	float sample_time;      // seconds
+	float current_torque;   // motor torque
+	bool use_torque;	// Use torque sensing
 
-        ros::ServiceClient dynamixel_client;
-        ros::Subscriber dynamixel_torque;
+	ros::ServiceClient dynamixel_client;
+	ros::Subscriber dynamixel_torque;
 
-    public:
-        DynamixelClient(ros::NodeHandle &nh, float input_speed, float input_depth, float input_time):
-                    nh_(nh),
-                    reel_speed(input_speed),
-                    desired_depth(input_depth),
-                    sample_time(input_time),
-                    current_torque(0),
-                    dynamixel_client(nh_.serviceClient<dynamixel_controllers::SetSpeed>("/reel_controller/set_speed")),
-                    dynamixel_torque(nh_.subscribe("/reel_controller/state", 100, &DynamixelClient::updateTorque, this))
+public:
+	DynamixelClient(ros::NodeHandle &nh, float input_speed, float input_depth, float input_time, bool ut):
+		nh_(nh),
+		reel_speed(input_speed),
+		desired_depth(input_depth),
+		sample_time(input_time),
+		current_torque(0),
+		use_torque(ut),
+		dynamixel_client(nh_.serviceClient<dynamixel_controllers::SetSpeed>("/reel_controller/set_speed")),
+		dynamixel_torque(nh_.subscribe("/reel_controller/state", 100, &DynamixelClient::updateTorque, this))
+	{}
 
-        {}
+	void actuateDynamixel(float input_depth = 0.0) 
+	{
+		dynamixel_controllers::SetSpeed temp_srv;
+		temp_srv.request.speed = -reel_speed;   // Negative speed lets out line
 
-        void actuateDynamixel() {
-            dynamixel_controllers::SetSpeed temp_srv;
-            temp_srv.request.speed = -reel_speed;   // Negative speed lets out line
+		desired_depth = input_depth;
 
-	    if (desired_depth > 0) { // goto depth
-                float duration = desired_depth / (reel_speed * 0.05);  // reel radius ~0.05ft
+		if (desired_depth > 0)  // goto depth
+		{
+			float duration = desired_depth / (reel_speed * 0.05);  // reel radius ~0.05ft
 
-                ROS_INFO("Lowering Sonde to %f depth", desired_depth);
-                dynamixel_client.call(temp_srv);
-                ros::Duration(duration).sleep();        // Lower Sonde to desired depth
+			ROS_INFO("Lowering Sonde to %f depth", desired_depth);
+			dynamixel_client.call(temp_srv);
+			ros::Duration(duration).sleep();        // Lower Sonde to desired depth
 
-                temp_srv.request.speed = 0.0;
-                dynamixel_client.call(temp_srv);        // Stop Sonde at desired Depth
-	    }
-
-	    if (desired_depth < 0) { // goto bottom
-		ROS_INFO("Lowering Sonde to bottom.");
-		dynamixel_client.call(temp_srv);
-		ros::Duration(2).sleep();
-	        while (current_torque < 0.15) { // 0.15 chosen arbitrarily
-		    ROS_INFO("Torque: %f", current_torque);
-    		    ros::spinOnce();
-  		    ros::Duration(0.5).sleep();
+			temp_srv.request.speed = 0.0;
+			dynamixel_client.call(temp_srv);        // Stop Sonde at desired Depth
 		}
+
+		if (desired_depth < 0) // goto bottom
+		{
+			ROS_INFO("Lowering Sonde to bottom.");
+			dynamixel_client.call(temp_srv);
+			ros::Duration(2).sleep();
+			while (current_torque < 0.15)  // 0.15 chosen arbitrarily
+			{
+				ROS_INFO("Torque: %f", current_torque);
+				ros::spinOnce();
+				ros::Duration(0.5).sleep();
+			}
+			temp_srv.request.speed = 0.0;
+			dynamixel_client.call(temp_srv);
+		}
+
+		ROS_INFO("Sonde lowered, sampling at specified depth.");
+		ros::Duration(sample_time).sleep();     // Let Sonde sample at desired depth
+
+		ROS_INFO("Raising Sonde.");
+		temp_srv.request.speed = reel_speed;   // Positive speed reels line in
+
+		//Raise for time:
+		dynamixel_client.call(temp_srv);       // Raise Sonde
+		//ros::Duration(duration).sleep();
+		// give it a moment to reverse torque sign
+
+		//Raise until torque:
+		while (current_torque > -0.18)     // 0.8 chosen arbitrarily
+		{
+			ROS_INFO("Torque: %f", current_torque);
+			ros::spinOnce();
+			ros::Duration(0.5).sleep();
+		}
+
+		//temp_srv.request.speed = 1;
+		//dynamixel_client.call(temp_srv);
+		//ros::Duration(3).sleep();
+
 		temp_srv.request.speed = 0.0;
 		dynamixel_client.call(temp_srv);
-	    }
+	}
 
-            ROS_INFO("Sonde lowered, sampling at specified depth.");
-            ros::Duration(sample_time).sleep();     // Let Sonde sample at desired depth
-
-            ROS_INFO("Raising Sonde.");
-            temp_srv.request.speed = reel_speed;   // Positive speed reels line in
-
-            //Raise for time:
-            dynamixel_client.call(temp_srv);       // Raise Sonde
-            //ros::Duration(duration).sleep();
-	     // give it a moment to reverse torque sign
-
-            //Raise until torque:
-            while (current_torque > -0.18) {          // 0.8 chosen arbitrarily
-                ROS_INFO("Torque: %f", current_torque);
-                ros::spinOnce();
-  		ros::Duration(0.5).sleep();
-            }
-
-	    //temp_srv.request.speed = 1;
-	    //dynamixel_client.call(temp_srv);
-	    //ros::Duration(3).sleep();
-
-            temp_srv.request.speed = 0.0;
-            dynamixel_client.call(temp_srv);
-        }
-
-        void updateTorque(const dynamixel_msgs::JointState& dynamixelState) {
-            current_torque = dynamixelState.load;
-        }
-};
+	void updateTorque(const dynamixel_msgs::JointState& dynamixelState) 
+	{
+		current_torque = dynamixelState.load;
+	}
+	
+}; // End Dynamixel class
 
 
 
@@ -169,9 +179,7 @@ public:
 		teensy_data_sub(nh.subscribe("teensy_data", 10, &CollectDataStateMachine::teensyDataCb, this)),
 		begin_collection_sub(nh.subscribe("begin_collection", 10, &CollectDataStateMachine::beginCollectionCb, this)),
 
-
-
-		dynamixel(nh_, 5.0, 0.0, 5.0) // 3rd param "desired_depth" 0.0 by default for now
+		dynamixel(nh_, 5.0, 0.0, 5.0, torque_sensing) // 3rd param "desired_depth" 0.0 by default for now
 
 	{
 		toTeensy.data = 0;
@@ -207,6 +215,8 @@ public:
 					teensy_state_pub.publish(toTeensy);
 					// NOW RUN DYNAMIXEL METHOD TO MOVE REEL?? TODO 
 					// If so, you'll also change to STATE 3 after that happens, should that all be here?
+
+					dynamixel.actuateDynamixel();
 
 					ROS_INFO("Switching to state 3");
 					STATE = 3;
