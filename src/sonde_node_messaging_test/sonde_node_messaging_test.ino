26a6795698eab5f47a23db3c7e2bf2fc97c8014b
Neil <nmccarter00@gmail.com>
2019-04-03 00:06:13 -0400

    store values in normal array, then move to message struct for sending, duhhh

--- src/sonde_node_messaging_test/sonde_node_messaging_test.ino
+++ src/sonde_node_messaging_test/sonde_node_messaging_test.ino
@@ -12,15 +12,6 @@
 
 #include <aro_refactored/sample.h>
 
-
-#include <SD.h>
-#include <SPI.h>
-File myFile;
-const int chipSelect = BUILTIN_SDCARD;
-Sd2Card card;
-SdVolume volume;
-SdFile root;
-
 ros::NodeHandle nh;
 
 // Messages
@@ -40,12 +31,13 @@ aro_refactored::sample samples[MAX_NUM_MSGS];
 int currMsgSize = 0;
 int numMsgs = 0;
 
-ros::Time stampBuffer[MAX_NUM_MSGS][MAX_MSG_SIZE];
-float dissolvedOxygenBuffer[MAX_NUM_MSGS][MAX_MSG_SIZE];
-float waterTempBuffer[MAX_NUM_MSGS][MAX_MSG_SIZE];
-float depthBuffer[MAX_NUM_MSGS][MAX_MSG_SIZE];
+aro_refactored::sample test;
 
-aro_refactored::sample toSend;
+
+ros::Time stampBuffer[MAX_MSG_SIZE];
+float dissolvedOxygenBuffer[MAX_MSG_SIZE];
+float waterTempBuffer[MAX_MSG_SIZE];
+float depthBuffer[MAX_MSG_SIZE];
 
 
 int STATE;
@@ -105,47 +97,11 @@ void setup() {
 
   Serial.begin(9600); // Arduino serial monitor
 
-  // Setting up SD card
-  if (!SD.begin(chipSelect)) {
-    Serial.println("SD initialization failed!");
-    return;
-  }
-  Serial.println("SD initialization done.");
 
-  //float timeForFile = float(nh.now().to_string());
-  // Convert this to date time format? Just use numbers for now
-
-  int fileNum = 0;
-  File root = SD.open("/");
-  root.rewindDirectory();
-  // Loop through files looking for largest one (all plaintext but no extension)
-  while (true) {
-    File entry = root.openNextFile();
-    if (!entry) {
-      Serial.println("No more files!");
-      break;
-    }
-    int entryNum = ((String)entry.name()).toInt();
-    Serial.println("File: " + (String)entryNum);
-    if (entryNum > fileNum) {
-      fileNum = entryNum;
-    }
-    entry.close();
-  }
-  root.close();
-  // Now need to make the next file 
-
-  fileNum++;
-  
-  char next[4]; // Make bigger if we think we will get more than this many files on SD card TODO
-  //next = (char*) &fileNum;
-  itoa(fileNum, next, 10);
-  Serial.println(fileNum);
-  Serial.println(next);
-  //myFile = SD.open((const char*)fileNum, FILE_WRITE);
-  myFile = SD.open(next, FILE_WRITE);
-
-  
+  Serial.println("Before");
+  //test.stamp = stampBuffer; // THIS WORKS
+  //test.stamp = samples[0].stamp; // SO DOES THIS
+  Serial.println("After");
 }
 
 
@@ -167,22 +123,6 @@ void loop() {
   //myFile.println("test");
   //myFile.flush();
 
-  switch (STATE)
-  {
-    case 0 : 
-          // Idle state, put a non-busy wait here
-          Serial.println("(state 0) Chilling");
-          delay(500); 
-          
-          break;
-    case 1 : 
-          // Sending heartbeats for testing comms
-          Serial.println("(state 1) Sending hb");
-          teensy_signal_pub.publish(&sig); // Change sig to something first? Value to indicate heartbeat? May help with debugging
-          delay(500);
-    
-          break;
-    case 2 : 
           // Start collecting data! Repeat until another signal arrives 
           //Serial.println("(state 2) Pretending to collect data brb");
           //delay(250);
@@ -217,19 +157,26 @@ void loop() {
 //          depthBuffer[currMsgSize] = depth;
 
           currTime = nh.now();
-          Serial.println("Before");
+          currTimeDouble = nh.now().toSec();
+          Serial.println(currTimeDouble);
+          /*Serial.println("Before");
           //samples[numMsgs].stamp[currMsgSize] = currTime;
-          stampBuffer[numMsgs][currMsgSize] = currTime;
           Serial.println("Before");
-          //samples[numMsgs].dissolvedOxygen[currMsgSize]  = DO;
-          dissolvedOxygenBuffer[numMsgs][currMsgSize] = DO;
+          samples[numMsgs].dissolvedOxygen[currMsgSize]  = DO;
           Serial.println("Before");
-          //samples[numMsgs].waterTemp[currMsgSize]  = temp;
-          waterTempBuffer[numMsgs][currMsgSize] = temp;
+          samples[numMsgs].waterTemp[currMsgSize]  = temp;
           Serial.println("Before");
-          //samples[numMsgs].depth[currMsgSize]  = depth;
-          depthBuffer[numMsgs][currMsgSize] = depth;
-          Serial.println("After");
+          samples[numMsgs].depth[currMsgSize]  = depth;
+          Serial.println("After");*/
+
+          Serial.println("Before");
+          //test.stamp[0] = currTime;
+          Serial.println("Before");
+          //test.dissolvedOxygen[0] = DO;
+          Serial.println("Before");
+          test.waterTemp[0] = temp;
+          Serial.println("Before");
+          test.depth[0] = depth;
           
           // Added to see what these data are
           //Serial.println(stampBuffer[currMsgSize]); Serial.println(dissolvedOxygenBuffer[currMsgSize]); Serial.println(waterTempBuffer[currMsgSize]); Serial.println(depthBuffer[currMsgSize]);    
@@ -237,9 +184,8 @@ void loop() {
           //Serial.print(DO);Serial.print("\t");Serial.print(temp);Serial.print("\t");Serial.println(depth);
           // End printing          
 
-          currTimeDouble = nh.now().toSec();
-          myFile.println((String)currTimeDouble + "\t" + (String)DO + "\t" + (String)temp + "\t" + (String)depth);
-          myFile.flush();
+          //myFile.println((String)currTimeDouble + "\t" + (String)DO + "\t" + (String)temp + "\t" + (String)depth);
+          //myFile.flush();
           
           ++currMsgSize;
     
@@ -267,17 +213,8 @@ void loop() {
           
           }
           //Serial.println(numMsgs);
-    
-          
-          break;
-    case 3 : 
-          // Sending heartbeats for testing comms
-          Serial.println("(state 3) Sending hb");
-          teensy_signal_pub.publish(&sig); // Change sig to something first? Value to indicate heartbeat? May help with debugging
-          delay(500);
-          
-          break;
-    case 4 : 
+
+
           // Send data
           Serial.println("(state 4) Sending data");
           Serial.println(numMsgs);
@@ -287,32 +224,24 @@ void loop() {
           for (int i = 0; i < numMsgs; ++i) 
           {
             Serial.print("Sending message ");
-            //teensy_data_pub.publish(&samples[i]);
-
-            toSend.stamp = stampBuffer[i];
-            toSend.dissolvedOxygen = dissolvedOxygenBuffer[i];
-            toSend.waterTemp = waterTempBuffer[i];
-            toSend.depth = depthBuffer[i];
-            
-            
-            teensy_data_pub.publish(&toSend);
+            teensy_data_pub.publish(&samples[i]);
             Serial.print("Sent message ");
             Serial.println(i);
             Serial.println("Printing contents");
-            /*for (int j = 0; j < MAX_MSG_SIZE; ++j) 
+            for (int j = 0; j < MAX_MSG_SIZE; ++j) 
             {
               Serial.print((String)samples[i].stamp[j].toSec() + "\t");
             }
-            Serial.println();*/
+            Serial.println();
           }
-          Serial.println("Clearing data"); // TODO TODO TODO 
+          Serial.println("Clearing data");
           //memset(samples, 0, sizeof(samples)); // This doesnt work, how overwrite old memory? Is necessary? TODO
           
           numMsgs = 0;
           currMsgSize = 0;
 
-          myFile.println("End sample");
-          myFile.flush();
+          //myFile.println("End sample");
+          //myFile.flush();
     
           // When done sending data, inform nuc // Here safe to assume messages will go thru (?)
           Serial.println("(state 4) Informing Nuc data transmission complete");
@@ -321,9 +250,9 @@ void loop() {
           // Change own state back to 0 i guess? // Actually nuc does it too, is redundant, choose one TODO
           Serial.println("(state 4) returning self to idle state");
           STATE = 0;
-          break;
+          //break;
       
-  }
+  //}
 
 
 
